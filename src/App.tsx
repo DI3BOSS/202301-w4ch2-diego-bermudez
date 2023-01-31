677097f7ab55b70cfe92521170ebb39b3393d6de
DI3BOSS <diegofbermudez@gmail.com>
2023-01-31 20:49:14 +0100

    Create early version of SearchComponent and add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import "./App.css";
+import SearchForm from "./components/SearchForm/SearchForm";
 
 function App() {
   return (
     <div className="container">
-      <form className="search-form">
-        <div className="row">
-          <label className="col-1 col-form-label"></label>
-          <div className="col-4">
-            <input type="text" id="search" className="search form-control" />
-          </div>
-          <button type="submit" className="btn btn-info btn-sm col-1">
-            Go
-          </button>
-        </div>
-      </form>
+      <SearchForm />
       <div className="row">
         <div className="container-gif offset-1 col-6"></div>
       </div>
